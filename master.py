be5e1b58d509ddc9715665c2bfef97bf41adfd1c
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-08-06 18:01:06 -0500

    write to csv file

--- master.py
+++ master.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import random
 import time
+import csv
 
 useragents = [
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
@@ -45,7 +46,7 @@ header = {
 }
 
 items = [
-    "https://www.amazon.com/ATHMILE-Quick-Dry-Barefoot-Exercise-Accessories/dp/B09Q3MYDQH/?_encoding=UTF8&pd_rd_w=4e3l3&content-id=amzn1.sym.5c57d321-b035-4d54-826c-1342435844f3&pf_rd_p=5c57d321-b035-4d54-826c-1342435844f3&pf_rd_r=D3XMV97P1WNMAJRXPN70&pd_rd_wg=ipopk&pd_rd_r=60972e67-b07a-47bf-8c4e-bcc9f5bbb382&ref_=pd_hp_d_btf_crs_zg_bs_7141123011",
+    "https://www.amazon.com/dp/1646091809/?coliid=I1R9351T70G1BR&colid=2F3AW09XJ6Z7S&psc=1&ref_=_sed_dp",
     "https://www.amazon.com/dp/1624650155/?coliid=IURSU0SKPC6YJ&colid=2F3AW09XJ6Z7S&psc=1&ref_=list_c_wl_gv_ov_lig_pi_dp",
     "https://www.amazon.com/HP-Students-Business-Quad-Core-Storage/dp/B0B2D77YB8/ref=sr_1_3?crid=AW0N7J626VW2&dib=eyJ2IjoiMSJ9.DPtOZwisQxLAXAf_kwR_161UU40DqEBUG-Ju057UXaDPU7mvyQL1dQdSw0VcrE9eZjB-FSNBPjs2k3d7nfeqOCqmG7r-GncabSJxAyKjHJ1tef_zILGpDagoX5F0cC1_RiSicVRU_BJYborbOzufN5InS807LFs_9dJAaXMWaReYkt4xwtesWSWed3K2mCOfvh1g0r90D6eZytlkh26xsAVR9oi5ugg6imqIZ4qtjy8.9DtrK2eO0ySrAeMZbTmdVP3PhHpuUfnQiSC9R5G-hxA&dib_tag=se&keywords=laptop&qid=1722726100&sprefix=laptop%2Caps%2C268&sr=8-3&th=1",
     "https://www.amazon.com/Neuromancer-William-Gibson/dp/0441007465/ref=sr_1_1?crid=369LZU17TQY1T&dib=eyJ2IjoiMSJ9.ArlChDCsKlOJy77lHBKtdqgVGLQyBiIWhiuMJDlV78yK4gG4tMY1z1L_LUpfjmHBX6EX5Jk-NsEMlYlSqBGhGeRaQjZE1H6zcxs1Vsilca7XsCL0Y15IpZE9TsPw3SA7-UPwELkgFje5_GqnW5eVq0D8iEUl6bHrAc7KWwHOBqmHtsou911CX4v5O6yiFoFByMObIuVtYOCQTpX7AtGtGwhUDjsx64_MXwwfpb95QFs.1V3YM80cWLbyiFvAcIR993jEzUNIHTCVA9SKsQsvz7Q&dib_tag=se&keywords=neuromancer&qid=1722726143&sprefix=neuromance%2Caps%2C139&sr=8-1",
@@ -77,7 +78,7 @@ def get_price(bs_obj):
         p = bs_obj.find("span", {"class": "a-price"}).span.text.strip()
     except:
         p=None
-
+    print(p)
     return p
 
 def get_image(bs_obj):
@@ -108,31 +109,26 @@ def get_info(page):
     return obj
 
 def clear_file():
-    with open('products.txt', 'w') as f:
-        f.write("")
+    with open('product_info.csv', 'w') as csv_file:
+        csv_file.write("")
 
 
-def output_info(obj):
-    with open('products.txt', 'a') as f:
-        line = ""
-        for key in obj:
-            line += obj[key] + " "
+def output_csv(product_list):
+    with open("product_info.csv", "a") as csv_file:
+        fieldnames = ["title", "image", "price", "rating"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
-        f.write(line + '\n')
-        f.close()
+        writer.writeheader()
+        for p in product_list:
+            writer.writerow(p)
 
+products = []
 
+clear_file()
+for item in items:
+    resp = connect(item)
+    soup = BeautifulSoup(resp.text, "html.parser").body
+    products += [get_info(soup)]
+    time.sleep(random.randrange(6, 30))
 
-while(True):
-    print("go")
-    clear_file()
-    for item in items:
-        resp = connect(item)
-        soup = BeautifulSoup(resp.text, "html.parser").body
-
-        product_info = get_info(soup)
-        output_info(product_info)
-        time.sleep(random.randrange(6, 30))
-
-    seconds = random.randrange(1800, 2200)
-    time.sleep(seconds)
+output_csv(products)
