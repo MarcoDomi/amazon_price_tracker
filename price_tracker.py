df2b280cb729954ef3da62be92cc8c3b8263c4ee
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-08-08 20:28:53 -0500

    add item to list if discount is over 10%

--- price_tracker.py
+++ price_tracker.py
@@ -4,28 +4,34 @@ import csv
 # TODO compare price in e/ object to e/ respective price in csv file to find discount
 # TODO if discount is at least 10% then send email
 
-def calculate_discount(master_price, current_price):
+def get_discount(master_price, current_price):
     discount_inverse = current_price/master_price
     discount = (1 - discount_inverse) * 100
+    
     return discount
 
 
 def get_discounted_products(current_products):
-    discounted_items = []
+    discounted_prodcuts = []
     with open("product_info.csv","r") as csv_file:
         product_index = 0
-        
+
         csv_reader = csv.DictReader(csv_file)
         for master_product in csv_reader:
             current_product = current_products[product_index]
+
             master_price = float(master_product["price"])
             current_price = float(current_product["price"])
-           
-           
+            if get_discount(master_price, current_price) > 10.0:
+                discounted_prodcuts += [current_product]
+
             product_index += 1
-            
+
+    return discounted_prodcuts
+
 
 if __name__ == "__main__":
     #current_products = web_scraper.get_product_info()
-    temp = [{"price": "17.75"}, {"price": "17.29"}, {"price": " 269.99"}, {"price": "11.19"}]
-    get_discounted_products(temp)
+    temp = [{"price": "1.75"}, {"price": "17.29"}, {"price": " 234.99"}, {"price": "11.19"}]
+    ds = get_discounted_products(temp)
+    print(ds)
