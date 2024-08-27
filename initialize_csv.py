8a2c40865278c52d26619a7155cb48dac8ace9e1
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-08-27 18:46:09 -0500

    read url from txt file

--- initialize_csv.py
+++ initialize_csv.py
@@ -1,6 +1,18 @@
 import web_scraper
 
 # only execute this script if product_info.csv is empty
-product_list = web_scraper.get_product_info()
-web_scraper.clear_csv()
-web_scraper.output_csv(product_list)
+
+#add file lines to a single string
+with open("item_urls.txt") as f:
+    url_str = ""
+    for line in f:
+        url_str += line
+        
+url_list = url_str.splitlines() #removes new line char at end of url
+
+if len(url_list) > 0:
+    product_list = web_scraper.get_product_info(url_list)
+    web_scraper.clear_csv()
+    web_scraper.output_csv(product_list)
+else:
+    print("URL file is empty.")
