802107d39e0a133d239565c722fc0d4bdf75d0b4
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-08-27 20:05:48 -0500

    rework reading lines from url file

--- initialize_csv.py
+++ initialize_csv.py
@@ -1,15 +1,12 @@
 import web_scraper
 
-# only execute this script if product_info.csv is empty
+#only execute this script if product_info.csv is empty
 
-#add file lines to a single string
+url_list = []
 with open("item_urls.txt") as f:
-    url_str = ""
     for line in f:
-        url_str += line
+        url_list.append(line.strip('\n'))
         
-url_list = url_str.splitlines() #removes new line char at end of url
-
 if len(url_list) > 0:
     product_list = web_scraper.get_product_info(url_list)
     web_scraper.clear_csv()
